9757c67f3416ba112d5b6b00572c312703dda98a
kauf0 <kauf0@pilgrim-realm.ru>
2024-12-04 12:31:37 +0900

    Implemented input scanner and `exit` command

--- src/main/java/com/github/kauf0/Calculator.java
+++ src/main/java/com/github/kauf0/Calculator.java
@@ -1,14 +1,8 @@
 package com.github.kauf0;
 
-import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
 import java.util.Scanner;
 
 public class Calculator {
-
-    public static final String CMD_EXIT = "cmd_exit";
-    public static final String CMD_COPY = "cmd_copy";
 
     public static void main(String[] args) {
         // Greet the user and give them instructions on usage
@@ -27,7 +21,7 @@ public class Calculator {
         if (input.equals(":q")) {
             System.exit(0);
         } else {
-            
+
         }
 
 
