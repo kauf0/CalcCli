7fc1e9e9d9e8b115148b9714ffdf27c4be39068a
kauf0 <kauf0@pilgrim-realm.ru>
2024-12-05 14:30:55 +0900

    Minor changes, commenting

--- src/main/java/com/github/kauf0/Calculator.java
+++ src/main/java/com/github/kauf0/Calculator.java
@@ -11,19 +11,22 @@ public class Calculator {
         // Greet the user and give them instructions on usage
         System.out.println("CalcCli\n" +
                 "Input ':q' to exit, or ':c' to copy output.");
+        // Create blank variables to use later
         String input = "";
 
+        // Math goes here
         do {
             // Create the scanner and read input Expression string
             Scanner scanner = new Scanner(System.in);
             input = scanner.nextLine();
-            //System.out.println(input); // this was for debugging purposes only
 
+            // Check for presence of commands and if expression doesn't contain
+            // any letters and special symbols
             if (input.equals(":q")){
                 System.exit(0);
             } else if (input.contains(":c")){
                 System.out.println("this should later be used to copy string");
-            } else if (input.matches("[a-zA-Z_;:\"'`~!@#$%{}\\[\\]]+") || input.isBlank()){
+            } else if (input.matches("[a-zA-Z;:\"'`~!@#$%{}\\[\\]]+") || input.isBlank()){
                 System.out.println("Please, provide proper input.");
             } else {
                 try {
