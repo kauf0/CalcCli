540ff61c03426fe3279b89f19170c82acf13f403
kauf0 <kauf0@pilgrim-realm.ru>
2024-12-05 12:42:41 +0900

    Evaluating math expressions
    Imported `EvalEx`
    Passing `scanner` input into `Expression input` and then evaluating it

--- src/main/java/com/github/kauf0/Calculator.java
+++ src/main/java/com/github/kauf0/Calculator.java
@@ -1,5 +1,12 @@
 package com.github.kauf0;
 
+import com.ezylang.evalex.EvaluationException;
+import com.ezylang.evalex.Expression;
+import com.ezylang.evalex.data.EvaluationValue;
+import com.ezylang.evalex.parser.ParseException;
+
+import javax.script.ScriptException;
+import java.beans.PropertyEditorSupport;
 import java.util.Scanner;
 
 public class Calculator {
@@ -8,22 +15,22 @@ public class Calculator {
         // Greet the user and give them instructions on usage
         System.out.println("CalcCli\n" +
                 "Input ':q' to exit, or ':c' to copy output.");
-        // Create blank output string and print it out
-        String output = "";
-        System.out.println(output);
-        // Create the scanner and read input string
+        // Create the scanner and read input Expression string
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        Expression input = new Expression(scanner.nextLine());
         System.out.println(input); // this is for debugging purposes only
 
-        // Check the input for quit and copy commands and, if
+        // Check the input for quit command and, if
         // not present, pass the string into parser
         if (input.equals(":q")) {
             System.exit(0);
         } else {
-
+            try {
+                EvaluationValue result = input.evaluate();
+                System.out.println(result.getNumberValue());
+            } catch (Exception e) {
+                e.printStackTrace(System.out);
+            }
         }
-
-
     }
 }
