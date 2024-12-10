9e83e0fa3c542ffb614fe7990de92ee79504b29e
kauf0 <101307532+kauf0@users.noreply.github.com>
2024-12-10 10:50:07 +0900

    Put everything in Switch-Case statement
    Now both Command processor and Evaluator are inside Switch-Case statement.

--- src/main/java/com/github/kauf0/Calculator.java
+++ src/main/java/com/github/kauf0/Calculator.java
@@ -23,19 +23,24 @@ public class Calculator {
 
             // Check for presence of commands and if expression doesn't contain
             // any letters and special symbols
-            if (input.equals(":q")){
-                System.exit(0);
-            } else if (input.contains(":c")){
-                System.out.println("this should later be used to copy string");
-            } else if (input.matches("[a-zA-Z;:\"'`~!@#$%{}\\[\\]]+") || input.isBlank()){
-                System.out.println("Please, provide proper input.");
-            } else {
-                try {
-                    Expression inputExp = new Expression(input);
-                    EvaluationValue result = inputExp.evaluate();
-                    System.out.println(result.getNumberValue().toPlainString());
-                } catch (Exception e) {
-                    e.printStackTrace(System.out);
+            switch (input) {
+                case String sInp when sInp.equals(":q") -> {
+                    System.exit(0);
+                }
+                case String sInp when sInp.contains(":c") -> {
+                    System.out.println("this should later be used to copy string");
+                }
+                case String sInp when sInp.matches("[a-zA-Z;:\"'`~!@#$%{}\\[\\]]+") || sInp.isBlank() -> {
+                    System.out.println("Please, provide proper input.");
+                }
+                default -> {
+                    try {
+                        Expression inputExp = new Expression(input);
+                        EvaluationValue result = inputExp.evaluate();
+                        System.out.println(result.getNumberValue().toPlainString());
+                    } catch (Exception e) {
+                        e.printStackTrace(System.out);
+                    }
                 }
             }
         } while (!input.equals(":q"));
