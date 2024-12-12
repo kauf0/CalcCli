520c633df2701f3aeedfef5ea966db585b51633a
kauf0 <101307532+kauf0@users.noreply.github.com>
2024-12-12 23:42:09 +0900

    Minor changes
    Fixed some warnings.

--- src/main/java/com/github/kauf0/Calculator.java
+++ src/main/java/com/github/kauf0/Calculator.java
@@ -13,9 +13,10 @@ public class Calculator {
 
     public static void main(String[] args) {
         // Greet the user and give them instructions on usage
-        System.out.println("CalcCli\n" +
-                "Input ':q' to exit.\n" +
-                ":h for list of commands.");
+        System.out.println("""
+                CalcCli
+                Input ':q' to exit.
+                :h for list of commands.""");
         // Create blank variables to use later
         String input = "";
 
@@ -27,6 +28,7 @@ public class Calculator {
 
             // Check for presence of commands and if expression doesn't contain
             // any letters and special symbols
+            final String inputCommandCutoff = input.substring(0, input.length() - 2);
             switch (input) {
                 // QUIT command
                 case String sInp when sInp.equals(":q") -> {
@@ -34,29 +36,30 @@ public class Calculator {
                 }
                 // HELP command
                 case String sInp when sInp.equals(":h") -> {
-                    System.out.println("List of commands:\n" +
-                            "| **These commands should be used at the end of expression**\n" +
-                            "| :q  - Quit\n" +
-                            "| :h  - Prints this message\n" +
-                            "|---------------------------------------------------------------\n" +
-                            "| :c  - Copies both the expression and the result\n" +
-                            "| :cr - Copies only the result\n" +
-                            "| :ce - Copies only the expression\n" +
-                            "|---------------------------------------------------------------\n" +
-                            "| :b  - Prints the result in true Big Decimal (without rounding)");
+                    System.out.println("""
+                            List of commands:
+                            | **These commands should be used at the end of expression**
+                            | :q  - Quit
+                            | :h  - Prints this message
+                            |---------------------------------------------------------------
+                            | :c  - Copies both the expression and the result
+                            | :cr - Copies only the result
+                            | :ce - Copies only the expression
+                            |---------------------------------------------------------------
+                            | :b  - Prints the result in true Big Decimal (without rounding)""");
                 }
                 // COPY command
                 case String sInp when sInp.endsWith(":c") -> {
                     try {
                         MathContext m = new MathContext(3);
                         // Cutting off the end of the sentence that contains ":c"
-                        Expression inputExp = new Expression(input.substring(0, input.length() - 2));
+                        Expression inputExp = new Expression(inputCommandCutoff);
 
                         EvaluationValue result = inputExp.evaluate();
                         System.out.println(result.getNumberValue().round(m).toPlainString());
 
                         // Copies both expression and result
-                        StringSelection stringSelection = new StringSelection(input.substring(0, input.length() -2) + "\n" + result.getNumberValue().round(m).toPlainString());
+                        StringSelection stringSelection = new StringSelection(inputCommandCutoff + "\n" + result.getNumberValue().round(m).toPlainString());
                         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
                         clipboard.setContents(stringSelection, null);
                     } catch (Exception e) {
@@ -103,7 +106,7 @@ public class Calculator {
                 case String sInp when sInp.endsWith(":b") -> {
                     try {
                         // Cutting off the end of the sentence that contains ":b"
-                        Expression inputExp = new Expression(input.substring(0, input.length() - 2));
+                        Expression inputExp = new Expression(inputCommandCutoff);
 
                         EvaluationValue result = inputExp.evaluate();
                         System.out.println(result.getNumberValue().toPlainString());
@@ -127,6 +130,6 @@ public class Calculator {
                     }
                 }
             }
-        } while (!input.equals(":q"));
+        } while (true);
     }
 }
